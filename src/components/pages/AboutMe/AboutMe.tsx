a9d7ba85dff24c902775b0740e2311ac2184eb41
Jonathan <jonathan.d.potter@outlook.com>
2022-05-22 11:15:59 -0400

    rearranged aboutMe

--- src/components/pages/AboutMe/AboutMe.tsx
+++ src/components/pages/AboutMe/AboutMe.tsx
@@ -2,52 +2,37 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Colors from "../../../Types/Enums/colors";
 import Button from "../../elements/Button/Button";
-import Subject from "../../elements/Subject/Subject";
 import "./AboutMe.scss";
 
 const AboutMe = () => {
   const navigate = useNavigate();
   return (
-    <div className="aboutme page">
-      <h2>About Me</h2>
-      <nav>
-        <Button
-          background={Colors.transBlack}
-          textColor={Colors.maize}
-          size={2}
-          text="Home"
-          rounded
-          onClick={() => navigate("/")}
-        />
-        <Button
-          background={Colors.transBlack}
-          textColor={Colors.maize}
-          size={2}
-          text="Projects"
-          rounded
-          onClick={() => navigate("/projects")}
-        />
-      </nav>
-      <section className="subjects">
-        <Subject>
-          <img src="/images/div1.jpg" alt="me" />
-        </Subject>
-        <Subject>
-          <img src="/images/webSelf.jpg" alt="me" />
-        </Subject>
-        <Subject>
-          <img src="/images/webSelfBW.jpg" alt="me" />
-        </Subject>
-        <Subject>
-          <img src="/images/div1.jpg" alt="me" />
-        </Subject>
-        <Subject>
-          <img src="/images/webSelf.jpg" alt="me" />
-        </Subject>
-        <Subject>
-          <img src="/images/webSelfBW.jpg" alt="me" />
-        </Subject>
-      </section>
+    <div className="about-me page">
+      <div className="opening">
+        <h2>About Me</h2>
+        <nav>
+          <Button
+            background={Colors.transBlack}
+            textColor={Colors.maize}
+            size={2}
+            text="Home"
+            rounded
+            onClick={() => navigate("/")}
+          />
+          <Button
+            background={Colors.transBlack}
+            textColor={Colors.maize}
+            size={2}
+            text="Projects"
+            rounded
+            onClick={() => navigate("/projects")}
+          />
+        </nav>
+      </div>
+      <div className="image1"></div>
+      <div className="divider"></div>
+      <div className="image2"></div>
+      <div className="divider"></div>
     </div>
   );
 };
